f3031a023e5094c65f985f114f4bf6abb1315880
GuillaumeWaignier <guillaume.waignier@gmail.com>
2020-08-10 15:50:27 +0200

    fix(scram): fix bug when using jaas parameter

--- config/start.sh
+++ config/start.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 base_dir=$(dirname $0)
 file="/config/config.properties"
@@ -14,15 +14,15 @@ for param in `env`
 do
   case ${param} in
     ${variable_prefix}* )
-      key=`echo ${param} | sed -e "s/^${variable_prefix}\([A-Za-z_0-9]*\)=.*/\1/g" | sed -e 's/_/./g' `
-      value=`echo ${param} | sed -e "s/^${variable_prefix}[A-Za-z_0-9]*=\(.*\)/\1/g" `
+      key=`echo ${param} | sed -e "s/^${variable_prefix}\([A-Za-z_0-9-]*\)=.*/\1/g" | sed -e 's/_/./g' `
+      value=`echo ${param} | sed -e "s/^${variable_prefix}[A-Za-z_0-9-]*=\(.*\)/\1/g" `
       echo ${key}=${value} >> ${file}
     ;;
   esac
 done
 
 
-if test ${DEBUG} = "true"
+if test "${DEBUG}" = "true"
 then
   echo "config file ${file} is"
   cat ${file}
